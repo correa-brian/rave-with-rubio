4898578fdf299e63767d6dd19b36d16cccbc8734
Brian <briancorrea@gmail.com>
2016-03-05 08:07:54 -0500

    Updated html/css/jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,37 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function(){
+	$("#menu-btn").click(function() {
+		$("#menu").toggle();
+	});
+
+	$("#about-btn").click(function() {
+		$('html, body').animate({
+			scrollTop:$("#about-section").offset().top
+		}, 800);
+	});
+
+	$("#events-btn").click(function() {
+		$('html, body').animate({
+			scrollTop:$("#events-section").offset().top
+		}, 800);
+	});
+
+	$('#autocomplete').autocomplete ({
+    lookup: states,
+    onSelect: function (suggestion) {
+        stateId = suggestion.value;
+        stateEvent = suggestion.data;
+        outputstring = 'Events in ' + suggestion.value + ". There are raves here: " + suggestion.data;
+        $('#output').append(outputstring)
+    	}
+	});
+
+	$(function() {
+		$("#datepicker").datepicker();
+		var date = $(this).datepicker('getDate');
+	});
+
+});
